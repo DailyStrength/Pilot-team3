e52bc34e574348470e62b522c66202fc19978ada
bookbook <jhkluck22@gmail.com>
2025-07-22 15:42:44 +0900

    Initial commit

---
+++ pilot-project/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+const count = ref(0);
+</script>
+
+<template>
+  <h1>App</h1>
+</template>
+
+<style scoped></style>
---
+++ vue3-job/src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Vue3 Job</h1>
+    <p>Welcome to your Vue.js app!</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div h1 {
+  color: #42b983;
+  text-align: center;
+  margin-top: 50px;
+  font-size: 2em;
+}
+</style>
