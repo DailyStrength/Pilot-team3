dee629799d68ffd635b8cf6dbd126a1481cda261
bookbook <jhkluck22@gmail.com>
2025-07-27 18:04:40 +0900

    라우터 수정

--- vue3-job/src/router.js/router.js
+++ vue3-job/src/router.js/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../view/Login.vue";
-import SignUp from "../view/Signup.vue";
+import SignUp from "../view/SignUp.vue";
 import JobList from "../view/JobList.vue";
 import JobDetail from "../view/JobDetail.vue";
 import UserProfile from "../view/UserProfile.vue";
